b217dff1f5b9950e6426c516dd2da3a2331af1c3
0Empty0 <garg.victor11@gmail.com>
2024-03-17 03:11:13 +0100

    add prepare header directive to api call

--- front-end/src/store/user/user-api.ts
+++ front-end/src/store/user/user-api.ts
@@ -1,9 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+
 export const UserApi = createApi({
   reducerPath: "user",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/api"
+    baseUrl,
+    prepareHeaders: (headers) => {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        headers.set('Authorization', token);
+      }
+      return headers;
+    },
   }),
   endpoints: (builder) => ({})
 })
